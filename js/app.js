5f52b46aa23af4b874cd0c581b1a7549db7a8674
Kevin Agreda <kevin.ale24@gmail.com>
2020-10-01 07:42:24 -0600

    Assign value to variable nameUser

--- js/app.js
+++ js/app.js
@@ -1,10 +1,13 @@
 const formName = document.getElementById("form-name");
 const inputName = document.getElementById("input-name");
+let nameUser = null;
+
 
 formName.addEventListener("submit", (ev) => {
     ev.preventDefault;
     if (inputName.value.length) {
         alert("Bienvenido " + inputName.value)
+        nameUser = inputName.value;
     } else 
         alert ("Nombre incompleto");
 })
