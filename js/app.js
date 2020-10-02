60c8a3c5a1735b9288285d3a9a19e7e4e12435db
Kevin Agreda <kevin.ale24@gmail.com>
2020-10-01 18:22:32 -0600

    Add the second section

--- js/app.js
+++ js/app.js
@@ -1,13 +1,28 @@
+const sections = document.querySelectorAll("main section");
 const formName = document.getElementById("form-name");
 const inputName = document.getElementById("input-name");
+const welcomeUser = document.getElementById("welcome-user");
+const btnReturn = document.getElementById("btn-return");
+
 let nameUser = null;
 
 
-formName.addEventListener("submit", (ev) => {
-    ev.preventDefault;
+formName.addEventListener("submit", (e) => {
+    e.preventDefault();
     if (inputName.value.length) {
-        alert("Bienvenido " + inputName.value)
         nameUser = inputName.value;
+        loadSecondSection();
     } else 
         alert ("Nombre incompleto");
-})
+});
+
+btnReturn.addEventListener("click",() =>{
+    sections[1].classList.add("hide");
+    sections[0].classList.remove("hide");
+});
+
+function loadSecondSection() {
+    sections[0].classList.add("hide");
+    sections[1].classList.remove("hide");
+    welcomeUser.innerText = `Bienvenido ${nameUser}`;
+}
