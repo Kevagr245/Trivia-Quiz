f54def4d7cddabd060141e4021023fde08aa92f2
Kevin Agreda <kevin.ale24@gmail.com>
2020-10-06 14:02:02 -0600

    Question bank start

---
+++ js/database.js
@@ -0,0 +1,56 @@
+const typeQuestion = [
+    {
+        id: 'sportQuestion',
+        img: 'sport.jpeg',
+        name: 'Deportes',
+        bank: [
+            {
+                question: '¿Qué jugador de fútbol ha ganado más campeonatos en la copa del mundo?',
+                answer1: 'Pelé, tres veces en 1958, 1962 y 1972',
+                answer2: 'Maradona, tres veces en 1982, 1990 y 1994',
+                answer3: 'Ronaldo Nazario, tres veces en 1994, 2002 y 2006',
+                answer4: 'Gerd Muller, tres veces en 1966, 1972, 1982',
+                correctAnswer: 0
+            },
+            {
+                question: '¿Qué jugador de la NBA ha ganado más campeonatos en toda la historia?',
+                answer1: 'Sam Jones, 11 campeonatos con los Celtics',
+                answer2: 'Bill Russell, 11 campeonatos con los Celtics',
+                answer3: 'Tom Heinsohn, 11 campeonatos con los Celtics',
+                answer4: 'K.C. Jones, 11 campeonatos con los Celtics',
+                correctAnswer: 1 
+            },
+            {
+                question: '¿Qué belga es considerado como uno de los mejores ciclistas de todos los tiempos?',
+                answer1: 'Rik Van Loopy',
+                answer2: 'Philippe Thys',
+                answer3: 'Lucien Van Impere',
+                answer4: 'Eddy Merchx',
+                correctAnswer: 3
+            },
+            {
+                question: '',
+                answer1: 'Myke Tyson',
+                answer2: '',
+                answer3: '',
+                answer4: '',
+                correctAnswer: 0
+            }
+        ]
+    },
+    {
+        id: 'videogameQuestion',
+        img: 'videogames.jpg',
+        name: 'Videojuegos',
+        bank: [
+            {
+                question: '',
+                answer1: '',
+                answer2: '',
+                answer3: '',
+                answer4: '',
+                correctAnswer: ''
+            }
+        ]
+    }
+];
